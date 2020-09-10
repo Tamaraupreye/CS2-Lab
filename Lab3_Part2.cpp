a374cd98a4d926e364ecb7852d07b2a2593415f2
Tamaraupreye <tamaraupreyebenni@gmail.com>
2020-09-10 14:44:03 +0000

    Updated

--- Lab3_Part2.cpp
+++ Lab3_Part2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -15,11 +16,11 @@ struct CourseFile {
     bool sorted = true;
     Course *info = new Course [fileSize];  // pointer to array
 
-    void addCourseRecord() {
+    void addCourse() {
         cout << "Enter Course Number: ";
         cin >> info[currPtr].number;
 
-        if (info[currPtr].number < info[currPtr - 1].number) {
+        if (currPtr >= 1 && info[currPtr].number < info[currPtr - 1].number) {
             sorted = false;
         }
 
@@ -35,7 +36,7 @@ struct CourseFile {
         cout << endl;
     }
 
-    void addMultipleCourseRecords() {
+    void addMultipleCourses() {
         int num;
 
         cout << "How many courses to add? ";
@@ -52,7 +53,7 @@ struct CourseFile {
         }
 
         for (int i = 0; i < num; i++) {
-            addCourseRecord();
+            addCourse();
         }
     }
 
@@ -91,7 +92,9 @@ struct CourseFile {
         if (sorted) {  // do a binary search
             c = binarySearch(0, currPtr, courseNo);
         }
-        else {  // do a linear search. makes sense to sort first then do binary search but code for merge sort is long
+        else {  
+            // do a linear search. makes sense to sort first then do binary search but code for merge sort is long
+            // maybe sort as entries are added so list is always corted
             c = linearSearch(0, currPtr, courseNo);
         }
         return c;
@@ -107,28 +110,100 @@ struct Instructor {
 };
 
 struct InstructorFile {
-    Instructor *info;  // pointer to array
-    int fileSize;
-    int currPtr;
+    int fileSize = 100;
+    int currPtr = 0;
+    bool sorted = true;
+    Instructor *info = new Instructor [fileSize];  // pointer to array
 
     void addInstructor() {
+        cout << "Enter Instructor ID: ";
+        cin >> info[currPtr].id;
 
+        if (currPtr >= 1 && info[currPtr].id < info[currPtr - 1].id) {
+            sorted = false;
+        }
+
+        cout << "Enter Instructor First Name: ";
+        cin >> info[currPtr].firstName;
+
+        cout << "Enter Instructor Last Name: ";
+        cin >> info[currPtr].lastName;
+
+        cout << "Enter Instructor Title: ";
+        cin.ignore();
+        getline(cin, info[currPtr].title);
+
+        cout << "Enter Instructor Department: ";
+        getline(cin, info[currPtr].department);
+
+        currPtr++;
+
+        cout << endl;
     }
 
     void addMultipleInstructors() {
-        
+        int num;
+
+        cout << "How many instructors to add? ";
+        cin >> num;
+
+        while (num + currPtr >= fileSize) {
+            Instructor *temp = new Instructor [2 * fileSize];
+            for (int i = 0; i < currPtr; i++) {
+                temp[i] = info[i];
+            }
+            delete [] info;
+            info = temp;
+            fileSize *= 2;
+        }
+
+        for (int i = 0; i < num; i++) {
+            addInstructor();
+        }
     }
 
     void printInstructors() {
-
+        for (int i = 0; i < currPtr; i++) {
+            cout << left << setw(3) << info[i].id << left << setw(20) << info[i].firstName + " "  + info[i].lastName << " ";
+            cout << left << setw(12) << info[i].title << left << setw(20) << info[i].department << endl;
+        }
+        cout << endl;
     }
 
-    int findInstructor() {
-        return 0;
+    Instructor * binarySearch(int l, int r, int num) {
+        if (r >= l) {
+            int m = (r + l)/2;
+            if (num == info[m].id) {
+                Instructor *i = &info[m];
+                return i;
+            }
+            else if (num > info[m].id) return binarySearch(m+1, r, num);
+            else return binarySearch(l, m-1, num);
+        }
+        return NULL;
     }
 
-    void modifyInstructor() {
-        
+    Instructor * linearSearch(int l, int r, int num) {
+        for (int i = l; i < r; i++) {
+            if (num == info[i].id) {
+                Instructor *x = &info[i];
+                return x;
+            }
+        }
+        return NULL;
+    }
+
+    Instructor * findInstructor(int instructorID) {
+        Instructor * i;
+        if (sorted) {  // do a binary search
+            i = binarySearch(0, currPtr, instructorID);
+        }
+        else {  
+            // do a linear search. makes sense to sort first then do binary search but code for merge sort is long
+            // maybe sort as entries are added so list is always corted
+            i = linearSearch(0, currPtr, instructorID);
+        }
+        return i;
     }
 };
 
@@ -143,35 +218,105 @@ struct CourseOffering {
 };
 
 struct CourseOfferingFile {
-    CourseOffering *info;  // pointer to array
-    int fileSize;
-    int currPtr;
+    int fileSize = 100;
+    int currPtr = 0;
+    CourseOffering *info = new CourseOffering [fileSize];  // pointer to array
 
-    void addCourseOffering() {
-
+    void addCourseOffering(CourseFile cf, InstructorFile _if) {
+        int id;
+        Instructor * instructor;
+        cout << "Enter Instructor ID: ";
+        cin >> id;
+        instructor = _if.findInstructor(id);
+        if (instructor == NULL) {
+            cout << "No instructor with id " << id << endl << endl;
+            return addCourseOffering(cf, _if);
+        }
+        else {
+            info[currPtr].instructor = instructor;
+        }
+        addCourseOfferingB(cf, _if);
     }
 
-    void addMultipleCourseOfferings() {
+    void addCourseOfferingB(CourseFile cf, InstructorFile _if) {
+        int courseNo;
+        Course * course;
+        cout << "Enter Course Number: ";
+        cin >> courseNo;
+        course = cf.findCourse(courseNo);
+        if (course == NULL) {
+            cout << "No course with number " << courseNo << endl << endl;
+            return addCourseOfferingB(cf, _if);
+        }
+        else {
+            info[currPtr].course = course;
+        }
 
+        cout << "Enter Year: ";
+        cin >> info[currPtr].year;
+
+        cout << "Enter Semester: ";
+        cin >> info[currPtr].semester;
+
+        cout << "Enter Section Number: ";
+        cin >> info[currPtr].sectionNumber;
+
+        cout << "Enter Time: ";
+        cin >> info[currPtr].time;
+
+        cout << "Enter Room: ";
+        cin.ignore();
+        getline(cin, info[currPtr].room);
+
+        currPtr++;
+
+        cout << endl;
+    }
+
+    void addMultipleCourseOfferings(CourseFile cf, InstructorFile _if) {
+        int num;
+
+        cout << "How many course offerings to add? ";
+        cin >> num;
+
+        while (num + currPtr >= fileSize) {
+            CourseOffering *temp = new CourseOffering [2 * fileSize];
+            for (int i = 0; i < currPtr; i++) {
+                temp[i] = info[i];
+            }
+            delete [] info;
+            info = temp;
+            fileSize *= 2;
+        }
+
+        for (int i = 0; i < num; i++) {
+            addCourseOffering(cf, _if);
+        }
     }
 
     void printCourseOfferings() {
-
-    }
-
-    int findCourseOffering() {
-        return 0;
-    }
-
-    void modifyCourseOffering() {
-        
+        for (int i = 0; i < currPtr; i++) {
+            cout << left << setw(20) << info[i].course->title;
+            cout << left << setw(8) << to_string(info[i].course->number)+"-"+to_string(info[i].sectionNumber);
+            cout << left << setw(20) << info[i].instructor->firstName+" "+info[i].instructor->lastName;
+            cout << left << setw(13) << info[i].semester+" "+to_string(info[i].year);
+            cout << left << setw(8) << info[i].time;
+            cout << left << setw(15) << info[i].room << endl; 
+        }
+        cout << endl;
     }
 };
 
 int main() {
     CourseFile courseFile;
-    courseFile.addMultipleCourseRecords();
+    courseFile.addMultipleCourses();
     courseFile.printCourses();
-    Course * c = courseFile.findCourse(136);
-    if (c != NULL) cout << c->title << endl << endl;
+
+    InstructorFile instructorFile;
+    instructorFile.addMultipleInstructors();
+    instructorFile.printInstructors();
+
+    CourseOfferingFile courseOfferingFile;
+    courseOfferingFile.addMultipleCourseOfferings(courseFile, instructorFile);
+    courseOfferingFile.printCourseOfferings();
 }
